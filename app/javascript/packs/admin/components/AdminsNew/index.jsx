60d2e110b6ccc41f58dc89af42dc2fd39bf339a8
Dang Nguyen <minhdang.net@gmail.com>
2017-09-10 08:41:31 +0700

    Redux Form 7 for AdminNew page
    Redux Form 7 for AdminNew page

--- app/javascript/packs/admin/components/AdminsNew/index.jsx
+++ app/javascript/packs/admin/components/AdminsNew/index.jsx
@@ -1,28 +1,92 @@
 import React from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Checkbox, Form, Message } from 'semantic-ui-react'
+import { Field, reduxForm } from 'redux-form'
+import compose from 'recompose/compose';
+import { connect } from 'react-redux';
+
+const validate = ({email}) => ({
+  email: !email && 'This field is required'
+})
+
+const renderField = ({
+  input,
+  type,
+  placeholder,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <input {...input} placeholder={placeholder} type={type} />
+    {
+      touched &&
+      (
+        (error && <Message error content={error} />) ||
+        (warning && <Message warning content={warning} />)
+      )
+    }
+  </div>
+)
 
 class AdminsNew extends React.Component {
   render() {
     return(
       <div>
-        <Form>
+        <Form error={!this.props.valid} onSubmit={this.props.handleSubmit(this.props.handleMySubmit)}>
           <Form.Field>
             <label>Email</label>
-            <input placeholder='Email' />
+            <Field
+              name="email"
+              component={renderField}
+              type="text"
+              placeholder="Email"
+            />
           </Form.Field>
           <Form.Field>
             <label>Password</label>
-            <input type='password' placeholder='Password' />
+            <Field
+              name="password"
+              component="input"
+              type="password"
+              placeholder="Password"
+            />
           </Form.Field>
           <Form.Field>
             <label>Password confirmation</label>
-            <input type='password' placeholder='Password confirmation' />
+            <Field
+              name="passwordConfirmation"
+              component="input"
+              type="password"
+              placeholder="Password confirmation"
+            />
           </Form.Field>
-          <Form.Button>Save</Form.Button>
+          <Form.Button type="submit" disabled={this.props.submitting}>Save</Form.Button>
         </Form>
       </div>
     )
   }
 }
 
-export default AdminsNew;
+const mapStateToProps = (state) => {
+  return {
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleMySubmit: (values) => { console.log(values); }
+  };
+};
+
+const enhance = compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  reduxForm(
+    {
+      form: 'AdminNew',
+      validate
+    },
+  )
+)
+
+export default enhance(AdminsNew);
--- app/javascript/packs/admin/state/rootReducer.js
+++ app/javascript/packs/admin/state/rootReducer.js
@@ -1,9 +1,11 @@
 import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import { reducers as apiReducers } from 'redux-api-call'
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
     routing: routerReducer,
+    form: formReducer,
     ...apiReducers
 })
 
