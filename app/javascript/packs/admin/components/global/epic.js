1dedc86b97bf670aabecb9e613dbe9bd9f55104a
Dang Nguyen <minhdang.net@gmail.com>
2017-09-27 21:22:45 +0700

    fix general message epic

--- app/javascript/packs/admin/components/global/epic.js
+++ app/javascript/packs/admin/components/global/epic.js
@@ -1,9 +1,17 @@
 import 'rxjs/add/operator/delay';
 import { updateMessagesVisisbleAction,
+         showGeneralMessageAction,
          UPDATE_AUTO_HIDDEN_MESSAGES_VISIBLE_ACTION } from './state';
 
 const hideAllMessages = () => (dispatch) => {
     dispatch(updateMessagesVisisbleAction(false));
+    dispatch(showGeneralMessageAction(
+        {
+            visible: false,
+            positive: true,
+            content: '',
+        }
+    ));
 };
 
 const isMessagesVisisbleTrue = action => action.type === UPDATE_AUTO_HIDDEN_MESSAGES_VISIBLE_ACTION
